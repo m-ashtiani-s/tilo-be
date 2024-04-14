8f40ece41514bb1aed3e8f3b2903c778b35221fb
mohammad <m.ashtiani.06s@gmail.com>
2024-04-14 11:44:53 +0330

    fix db bug

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
-const connectDB = require("./config/db");
 const webRouterRouter = require("./routes/api");
+const mongoose = require("mongoose");
+const app = require("express")();
 
+mongoose.connect("mongodb://127.0.0.1:27017/tilo-db");
+mongoose.Promise = global.Promise;
 
-const app = express();
-app.use(cors({
-    origin:'http://localhost:3000',
-    credentials:true
-}));
-app.use(express.json())
-app.use(cookieParser())
-app.use('/api',webRouterRouter)
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		credentials: true,
+	})
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api", webRouterRouter);
 
-const PORT = 8000 || process.env.PORT;
-connectDB().then(() => {
-	app.listen(PORT, () => {
-		console.log("server running");
-	});
+app.listen(8000, () => {
+	console.log(`server running on port ${8000}`);
 });
